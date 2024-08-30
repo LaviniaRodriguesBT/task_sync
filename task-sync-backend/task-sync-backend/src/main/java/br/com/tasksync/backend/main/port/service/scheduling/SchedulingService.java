098ae28def06976b8f8348c1eba32c86d295b6fc
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-30 11:03:13 -0300

    feat: adicao das interfaces de event, user, scheduling e task na pasta de service

---
+++ task-sync-backend/task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/scheduling/SchedulingService.java
@@ -0,0 +1,7 @@
+package br.com.tasksync.backend.main.port.service.scheduling;
+
+import br.com.tasksync.backend.main.domain.SchedulingModel;
+import br.com.tasksync.backend.main.port.service.crud.CrudService;
+
+public interface SchedulingService extends CrudService<SchedulingModel>{
+}
---
+++ task-sync-backend/task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/user/UserService.java
@@ -0,0 +1,7 @@
+package br.com.tasksync.backend.main.port.service.user;
+
+import br.com.tasksync.backend.main.domain.UserModel;
+import br.com.tasksync.backend.main.port.service.crud.CrudService;
+
+public interface UserService extends CrudService<UserModel> {
+}
