b775f75dca53aa519ad6c7a643310f5e92e8f790
MichelecFernandes <michelecristinafr@gmail.com>
2024-08-30 09:54:33 -0300

    feat: criando a chamada do BackendApplication

---
+++ task-sync-backend/task-sync-backend/src/test/java/br/com/tasksync/backend/app/BackendAppicationTests.java
@@ -0,0 +1,10 @@
+package br.com.tasksync.backend.app;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BackendAppicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
