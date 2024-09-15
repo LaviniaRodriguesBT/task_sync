3727138401e2b97ddf243bcd37833708536de676
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-15 19:41:57 -0300

    help: definicao da classe UserRestController.java

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/controller/UserRestController.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/controller/UserRestController.java
@@ -8,9 +8,13 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+//@RestController indica que os metodos irao responder a requisições HTTPs
+//@RequestMapping define o caminho para qual todas as requições para o o endereço "api/event" deveão executar alguns dos metodos da classe
 
 @RestController
 @RequestMapping("/api/user")
+//Classe responsavel por traduz as requições HTTP de seus metodos (GET,POST,PUT,DELETE)
+// E atraves dessa traducao, retornar as respostas adequadas
 public class UserRestController {
     private final UserService userService;
 
