76fb3a440bad5d7f63460ea659e0745a756adca6
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-16 22:40:14 -0300

    help: definicao da funcionalidades da classe

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/crud/CreateService.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/crud/CreateService.java
@@ -1,5 +1,7 @@
 package br.com.tasksync.backend.main.port.service.crud;
 
+
+//Classe de serviço responsavel por criar uma nova entidade do tipo passado(T)
 public interface CreateService<T> {
     int create(final T entity);
 
