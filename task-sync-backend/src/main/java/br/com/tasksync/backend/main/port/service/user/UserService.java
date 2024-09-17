1b7c4628d7291b3bc840d4d9f7b1674f7a798b68
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-16 22:19:25 -0300

    help: correcao de erros de ortografia

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/user/UserService.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/user/UserService.java
@@ -3,5 +3,6 @@ package br.com.tasksync.backend.main.port.service.user;
 import br.com.tasksync.backend.main.domain.UserModel;
 import br.com.tasksync.backend.main.port.service.crud.CrudService;
 
+//Classe de serviço responsavel por chamar todos os CRUDs
 public interface UserService extends CrudService<UserModel> {
 }
