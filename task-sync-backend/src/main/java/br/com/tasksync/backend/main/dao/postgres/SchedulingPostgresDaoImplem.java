69eccd01bb8c04be01e9b678a6de24ee545a984e
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-18 18:51:37 -0300

    feat: ajustes na requisicao no postman de scheduling

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/dao/postgres/SchedulingPostgresDaoImplem.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/dao/postgres/SchedulingPostgresDaoImplem.java
@@ -11,70 +11,49 @@ import java.util.logging.Logger;
 
 public class SchedulingPostgresDaoImplem implements SchedulingDao {
 
-
     private static final Logger logger = Logger.getLogger(SchedulingPostgresDaoImplem.class.getName());
-
     private final Connection connection;
 
     public SchedulingPostgresDaoImplem(Connection connection) {
         this.connection = connection;
     }
 
-
     @Override
     public int add(SchedulingModel entity) {
-
         String sql = "INSERT INTO scheduling(start_time, end_time, date, status) ";
         sql += " VALUES(?, ?, ?, ?);";
-
         PreparedStatement preparedStatement;
         ResultSet resultSet;
-
         try {
             connection.setAutoCommit(false);
-
             preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-
-
             preparedStatement.setDate(1, Date.valueOf(entity.getStart_time()));
             preparedStatement.setDate(2, Date.valueOf(entity.getEnd_time()));
             preparedStatement.setDate(3, Date.valueOf(entity.getDate()));
             preparedStatement.setString(4, entity.getStatus());
-
             preparedStatement.execute();
-
             resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 final int id = resultSet.getInt(1);
                 entity.setId(id);
-
             } else {
                 throw new RuntimeException();
-
             }
 
             resultSet.close();
             preparedStatement.close();
-
-
             connection.commit();
-
             resultSet.close();
             preparedStatement.close();
 
         } catch (SQLException e) {
-
             try {
                 connection.rollback();
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
-
-
             throw new RuntimeException(e);
         }
-
-
         return entity.getId();
     }
 
@@ -82,15 +61,12 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
     public void remove(int id) {
         logger.log(Level.INFO, "Preparadando para remover a entidade com id " + id);
         final String sql = "DELETE FROM scheduling WHERE id = ?;";
-
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             preparedStatement.execute();
             preparedStatement.close();
             logger.log(Level.INFO, "Entidade removida com sucesso");
-
-
         } catch (Exception e) {
             throw new RuntimeException();
         }
@@ -100,10 +76,8 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
     @Override
     public SchedulingModel readyById(int id) {
         final String sql = "SELECT * FROM scheduling WHERE id = ?;";
-
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-
         try {
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
@@ -111,14 +85,11 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
             if (resultSet.next()) {
                 final SchedulingModel scheduling = new SchedulingModel();
                 scheduling.setId(resultSet.getInt("id"));
-
-                scheduling.setDate(resultSet.getDate("start_time").toLocalDate());
-                scheduling.setDate(resultSet.getDate("end_time").toLocalDate());
+                scheduling.setStart_time(resultSet.getDate("start_time").toLocalDate());
+                scheduling.setEnd_time(resultSet.getDate("end_time").toLocalDate());
                 scheduling.setDate(resultSet.getDate("date").toLocalDate());
                 scheduling.setStatus(resultSet.getString("status"));
-
                 logger.log(Level.INFO, "Entidade com id " + id + "encontrada com sucesso");
-
                 return scheduling;
 
             }
@@ -142,21 +113,22 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
     public List<SchedulingModel> readAll() {
 
         final List<SchedulingModel> schedulings = new ArrayList<>();
-        final String sql = "SELECT * FROM scheduling;";
-
-
+        final String sql = "SELECT s.*, e.name, e.id as event_id, c.user_id " +
+                "FROM scheduling s " +
+                "INNER JOIN contract c ON c.id = s.contract_id " +
+                "INNER JOIN \"event\" e ON e.id = c.event_id;";
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 final SchedulingModel scheduling = new SchedulingModel();
-
                 scheduling.setId(resultSet.getInt("id"));
                 scheduling.setDate(resultSet.getDate("date").toLocalDate());
-                scheduling.setDate(resultSet.getDate("start_time").toLocalDate());
-                scheduling.setDate(resultSet.getDate("end_time").toLocalDate());
+                scheduling.setStart_time(resultSet.getDate("start_time").toLocalDate());
+                scheduling.setEnd_time(resultSet.getDate("end_time").toLocalDate());
+                scheduling.setEvent_id(resultSet.getInt("event_id"));
+                scheduling.setEvent(resultSet.getString("name"));
                 schedulings.add(scheduling);
-
             }
             resultSet.close();
             preparedStatement.close();
@@ -169,7 +141,6 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
     @Override
     public void updateInformation(int id, SchedulingModel entity) {
         String sql = "UPDATE scheduling SET name = ? WHERE id = ?;";
-
         try {
             PreparedStatement preparedStatement;
             preparedStatement = connection.prepareStatement(sql);
@@ -180,8 +151,5 @@ public class SchedulingPostgresDaoImplem implements SchedulingDao {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
     }
-
-
 }
--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/dao/postgres/TaskPostgresDaoImplem.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/dao/postgres/TaskPostgresDaoImplem.java
@@ -14,67 +14,47 @@ import java.util.logging.Logger;
 
 public class TaskPostgresDaoImplem implements TaskDao {
 
-
     private static final Logger logger = Logger.getLogger(TaskPostgresDaoImplem.class.getName());
-
     private final Connection connection;
 
     public TaskPostgresDaoImplem(Connection connection) {
         this.connection = connection;
     }
 
-
     @Override
     public int add(TaskModel entity) {
 
         String sql = "INSERT INTO task(name) ";
         sql += " VALUES(?);";
-
         PreparedStatement preparedStatement;
         ResultSet resultSet;
 
         try {
             connection.setAutoCommit(false);
-
             preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-
             preparedStatement.setString(1, entity.getName());
-
-
             preparedStatement.execute();
-
             resultSet = preparedStatement.getGeneratedKeys();
             if (resultSet.next()) {
                 final int id = resultSet.getInt(1);
                 entity.setId(id);
-
             } else {
                 throw new RuntimeException();
-
             }
-
             resultSet.close();
             preparedStatement.close();
-
-
             connection.commit();
-
             resultSet.close();
             preparedStatement.close();
 
         } catch (SQLException e) {
-
             try {
                 connection.rollback();
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
-
-
             throw new RuntimeException(e);
         }
-
-
         return entity.getId();
     }
 
@@ -82,15 +62,12 @@ public class TaskPostgresDaoImplem implements TaskDao {
     public void remove(int id) {
         logger.log(Level.INFO, "Preparadando para remover a entidade com id " + id);
         final String sql = "DELETE FROM task WHERE id = ?;";
-
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
             preparedStatement.execute();
             preparedStatement.close();
             logger.log(Level.INFO, "Entidade removida com sucesso");
-
-
         } catch (Exception e) {
             throw new RuntimeException();
         }
@@ -100,10 +77,8 @@ public class TaskPostgresDaoImplem implements TaskDao {
     @Override
     public TaskModel readyById(int id) {
         final String sql = "SELECT * FROM task WHERE id = ?;";
-
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-
         try {
             preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setInt(1, id);
@@ -112,15 +87,10 @@ public class TaskPostgresDaoImplem implements TaskDao {
                 final TaskModel task = new TaskModel();
                 task.setId(resultSet.getInt("id"));
                 task.setName(resultSet.getString("name"));
-
-
                 logger.log(Level.INFO, "Entidade com id " + id + "encontrada com sucesso");
-
                 return task;
 
             }
-
-
             return null;
         } catch (Exception e) {
             throw new RuntimeException(e);
