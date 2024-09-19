4c38367ce92d6dbd57d3d1780fe816a1f0ad1f18
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-19 19:08:20 -0300

    feat: adicao do @CrossOrigin para permitir a comunicacao do bacn com o front

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/controller/EventRestController.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/controller/EventRestController.java
@@ -25,35 +25,39 @@ public class EventRestController {
         this.eventService = eventService;
     }
 
+    @CrossOrigin
     @GetMapping()
-    public ResponseEntity<List<EventModel>> getEntities(){
+    public ResponseEntity<List<EventModel>> getEntities() {
         List<EventModel> events = eventService.findAll();
         return ResponseEntity.ok().body(events);
     }
 
-
+    @CrossOrigin
     @GetMapping("/{id}")
-    public ResponseEntity<EventModel> getEntityById(@PathVariable final int id){
+    public ResponseEntity<EventModel> getEntityById(@PathVariable final int id) {
         EventModel event = eventService.findById(id);
         return ResponseEntity.ok().body(event);
     }
 
+    @CrossOrigin
     @PostMapping()
-    public ResponseEntity<EventModel> createEntity(@RequestBody final EventModel data){
+    public ResponseEntity<EventModel> createEntity(@RequestBody final EventModel data) {
         int id = eventService.create(data);
         final URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequestUri().path("{id}").buildAndExpand(id).toUri();
         return ResponseEntity.created(uri).build();
     }
 
+    @CrossOrigin
     @PutMapping("/{id}")
-    public ResponseEntity<Void> updateEntity(@PathVariable final int id, @RequestBody final EventModel data){
+    public ResponseEntity<Void> updateEntity(@PathVariable final int id, @RequestBody final EventModel data) {
         eventService.update(id, data);
         return ResponseEntity.ok().build();
     }
 
+    @CrossOrigin
     @DeleteMapping("/{id}")
-    public ResponseEntity<EventModel> deleteEntity(@PathVariable final int id){
+    public ResponseEntity<EventModel> deleteEntity(@PathVariable final int id) {
         eventService.delete(id);
         return ResponseEntity.noContent().build();
     }
