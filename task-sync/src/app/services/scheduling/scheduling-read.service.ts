41604cb4bdfabf620c1a8159d9e34cac9fbd82bd
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-28 11:16:59 -0300

    refactor: correcao na service para buscar o dto de ReponseScheduling

--- task-sync/src/app/services/scheduling/scheduling-read.service.ts
+++ task-sync/src/app/services/scheduling/scheduling-read.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
 import { Scheduling } from '../../domain/model/scheduling.model';
 import { SchedulingUser } from '../../domain/model/scheduling_user.model';
+import { ResponseScheduling } from '../../domain/dto/response-scheduling';
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +16,15 @@ export class SchedulingReadService {
     return firstValueFrom(this.http.get<Scheduling>(`http://localhost:8080/api/event/scheduling/${id}`));
   }
 
-  findByEventId(id: string): Promise<SchedulingUser[]>{
-    return firstValueFrom(this.http.get<SchedulingUser[]>(`http://localhost:8080/api/event/scheduling?event_id=${id}`));
+  findByEventId(id: string): Promise<ResponseScheduling[]>{
+    return firstValueFrom(this.http.get<ResponseScheduling[]>(`http://localhost:8080/api/event/scheduling?event_id=${id}`));
   }
 
   findByName(name: string): Promise<Scheduling[]> {
     return firstValueFrom(this.http.get<Scheduling[]>(`http://localhost:8080/api/event/scheduling?name=${name}`));
   }
 
-  findAll(): Promise<Scheduling[]>{
-    return firstValueFrom(this.http.get<Scheduling[]>('http://localhost:8080/api/event/scheduling'));
+  findAll(): Promise<ResponseScheduling[]>{
+    return firstValueFrom(this.http.get<ResponseScheduling[]>('http://localhost:8080/api/event/scheduling'));
   }
 }
