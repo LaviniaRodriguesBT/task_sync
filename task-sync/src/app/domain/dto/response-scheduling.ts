19752467110e65c8f126a29c07cd19f7aa3862bd
MichelecFernandes <michelecristinafr@gmail.com>
2024-09-28 11:10:26 -0300

    refactor: adicao do responseScheduling no arquivo do front para fazer as chamadas de cronogramas do evento

---
+++ task-sync/src/app/domain/dto/response-scheduling.ts
@@ -0,0 +1,15 @@
+import { Task } from "../model/task.model";
+import { User } from "../model/user.model";
+import { Event } from "../model/event.model";
+
+export interface ResponseScheduling {
+    id: string;
+    event: Event,
+    user: User,
+    task: Task,
+    value: string,
+    start_time: string,
+    end_time: string,
+    date: string,
+    status: string,
+}
