be83046295ce8fe2f8a24a8477ac8ece02b1ea07
MichelecFernandes <michelecristinafr@gmail.com>
2024-10-04 20:59:28 -0300

    feat: correcao de exportar o arquivo como excel

--- task-sync/src/app/views/app/task/task-list/task-list.component.ts
+++ task-sync/src/app/views/app/task/task-list/task-list.component.ts
@@ -9,6 +9,7 @@ import * as fontawesome from '@fortawesome/free-solid-svg-icons';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { IgxExcelExporterOptions, IgxExcelExporterService } from 'igniteui-angular';
 
 @Component({
   selector: 'task-sync-task-list',
@@ -38,6 +39,7 @@ export class TaskListComponent implements OnInit {
     private taskReadService: TaskReadService,
     private taskDeleteService: TaskDeleteService,
     private toastrService: ToastrService,
+    private excelExporter: IgxExcelExporterService,
    
   ) {
     this.accessType = localStorage.getItem('accessType')
@@ -75,6 +77,10 @@ export class TaskListComponent implements OnInit {
     window.print()
   }
 
+  public exportExcelEventList() {
+    this.excelExporter.exportData(this.tasks, new IgxExcelExporterOptions('ExportedDataFile'));
+  
+  }
 
 
   previousPage() {
