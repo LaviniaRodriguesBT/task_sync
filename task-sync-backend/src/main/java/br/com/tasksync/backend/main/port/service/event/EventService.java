a50b0347e5ca0ff6ff1271a793fe9281b9301a4e
MichelecFernandes <michelecristinafr@gmail.com>
2024-10-08 21:50:35 -0300

    feat: adicao do metodo reponsavel por buscar a  lista de eventos que estejam associados ao usuario que foi passado

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/event/EventService.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/service/event/EventService.java
@@ -3,6 +3,9 @@ package br.com.tasksync.backend.main.port.service.event;
 import br.com.tasksync.backend.main.domain.EventModel;
 import br.com.tasksync.backend.main.port.service.crud.CrudService;
 
+import java.util.List;
+
 //Classe de serviço responsavel por chamar todos os CRUDs
 public interface EventService extends CrudService<EventModel> {
+    public List<EventModel> getEntitiesByUserId(final  int id);
 }
