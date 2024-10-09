36a80a82153ef5eb1e09f9d35dc3059a4062665f
MichelecFernandes <michelecristinafr@gmail.com>
2024-10-08 21:47:12 -0300

    feat: correção do get responsavel por buscar no banco o tipo de acesso do usuario (80%)

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/dao/event/EventDao.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/dao/event/EventDao.java
@@ -3,5 +3,9 @@ package br.com.tasksync.backend.main.port.dao.event;
 import br.com.tasksync.backend.main.domain.EventModel;
 import br.com.tasksync.backend.main.port.dao.crud.CrudDao;
 
+import java.util.List;
+
 public interface EventDao extends CrudDao<EventModel> {
+    public List<EventModel> getEntitiesByUserId(final  int id);
+
 }
