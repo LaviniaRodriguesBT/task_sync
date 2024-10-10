ee0cafa0731786d19c9974b4fded9b0f47df9abb
LaviniaRodriguesBT <laviniarodriguesbt@gmail.com.br>
2024-10-09 22:29:44 -0300

    feat: implementacao dos botoes para acessar os diagramas e as documentacoes + adicao do contato

--- task-sync/src/app/views/site-informativo/site-informativo.component.ts
+++ task-sync/src/app/views/site-informativo/site-informativo.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatExpansionPanel, MatExpansionModule } from '@angular/material/expansion'; 
 
 @Component({
   selector: 'task-sync-site-informativo',
   standalone: true,
-  imports: [],
+  imports: [
+    MatExpansionModule,
+    CommonModule
+  ], 
   templateUrl: './site-informativo.component.html',
-  styleUrl: './site-informativo.component.css'
+  styleUrls: ['./site-informativo.component.css']
 })
 export class SiteInformativoComponent {
+  
+  navigateTo(url: string) {
+    window.open(url, '_blank'); 
+  }
+
+  isModalOpen = false;
+  selectedImage: string | null = null;
+
+  openModal(imagePath: string) {
+    this.selectedImage = imagePath;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedImage = null;
+  }
+  
+  isExpanded = false;
+
+  onExpandChange(event: { expanded: boolean }) {
+    this.isExpanded = event.expanded;
+}
 
 }
