4527373f2206ae68745257d7a4ab4dbe145733ba
LaviniaRodriguesBT <laviniarodriguesbt@gmail.com.br>
2024-10-17 09:37:50 -0300

    feat: adicao da validacao de senha forte

--- task-sync/src/app/views/app/user/user-create/user-create.component.ts
+++ task-sync/src/app/views/app/user/user-create/user-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { User } from '../../../../domain/model/user.model';
 import { FormsModule, ReactiveFormsModule, FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+
 @Component({
   selector: 'task-sync-user-create',
   standalone: true,
@@ -25,38 +26,56 @@ import { MatSelectModule } from '@angular/material/select';
   templateUrl: './user-create.component.html',
   styleUrl: './user-create.component.css'
 })
+
 export class UserCreateComponent implements OnInit {
+
+  @ViewChild('imagePreview') imagePreview!: ElementRef<HTMLImageElement>;
+  @ViewChild('imgUser') imgUser!: ElementRef<HTMLInputElement>;
+  @ViewChild('dateInput') dateInput!: ElementRef<HTMLInputElement>;
+
+
   form!: FormGroup;
+
   nameMinLength: number = 3;
+
   emailMinLength: number = 15;
+
   addressMinLength: number = 10;
+
   passwordMinLength: number = 6;
+
   descriptionMinValue: number = 3;
+
   selectedImage: File | null = null;
   fileName: string = 'Nenhum arquivo escolhido';
   showImagePreview: boolean = false;
   image!: string;
+
   constructor(
     private formBuilder: FormBuilder,
     private toastr: ToastrService,
     private router: Router,
     private userCreateService: UserCreateService,
   ) {
+
     this.initializeForm();
   }
+
   ngOnInit(): void {
   }
+
   initializeForm() {
     this.form = this.formBuilder.group({
       email: ['', [Validators.required, Validators.minLength(this.emailMinLength), Validators.maxLength(200)]],
-      password: ['', [Validators.required, Validators.minLength(this.passwordMinLength), Validators.maxLength(500)]],
+      password: ['', [Validators.required, Validators.minLength(this.passwordMinLength), Validators.maxLength(20)]],
       cpf: ['', [Validators.required, this.cpfValidator]],
       name: ['', [Validators.required, Validators.minLength(this.nameMinLength), Validators.maxLength(200)]],
-      address: ['', [Validators.required, Validators.minLength(this.addressMinLength), Validators.maxLength(400)]],
+      address: ['', [Validators.required, Validators.minLength(this.addressMinLength), Validators.maxLength(200)]],
       phone: ['', [Validators.required, this.phoneValidator]],
       access_type: ['', [Validators.required]],
     });
   }
+
   validateNumber(event: KeyboardEvent) {
     const charCode = event.keyCode ? event.keyCode : event.which;
     if (charCode < 48 || charCode > 57) {
@@ -65,7 +84,7 @@ export class UserCreateComponent implements OnInit {
   }
   cpfValidator(control: AbstractControl): { [key: string]: boolean } | null {
     const value = control.value || '';
-    const cpfPattern = /^[0-9]{14}$/;
+    const cpfPattern = /^[0-9]{11}$/;
     if (!cpfPattern.test(value)) {
       return { invalidCPF: true };
     }
@@ -73,22 +92,13 @@ export class UserCreateComponent implements OnInit {
   }
   phoneValidator(control: AbstractControl): { [key: string]: boolean } | null {
     const value = control.value || '';
-    const phonePattern = /^[0-9]{11,14}$/;
+    const phonePattern = /^[0-9]{10,11}$/;
     if (!phonePattern.test(value)) {
       return { invalidPhone: true };
     }
     return null;
   }
-  showPassword: boolean = false;
-  togglePasswordVisibility(): void {
-    this.showPassword = !this.showPassword;
-    const passwordField = document.getElementById('password') as HTMLInputElement;
-    if (this.showPassword) {
-      passwordField.type = 'text';
-    } else {
-      passwordField.type = 'password';
-    }
-  }
+
   async create() {
     const user: User = {
       email: this.form.controls['email'].value,
@@ -99,9 +109,11 @@ export class UserCreateComponent implements OnInit {
       phone: this.form.controls['phone'].value,
       access_type: this.form.controls['access_type'].value,
       image: this.image,
-    }
+    };
+
     console.log('preparando para criar a pessoa...');
     console.log(user);
+
     try {
       await this.userCreateService.createUser(user);
       this.toastr.success('Dados salvos com sucesso!');
@@ -110,19 +122,20 @@ export class UserCreateComponent implements OnInit {
       this.toastr.error(error.message);
     }
   }
+
   onImageSelected(user: any) {
     const file = user.target.files[0];
     if (file) {
       this.selectedImage = file;
       this.fileName = file.name;
       this.showImagePreview = true;
+
       const reader = new FileReader();
-      reader.onload = (e) => {
+      reader.onload = () => {
         this.image = reader.result as string;
-        const img = document.getElementById('image-preview') as HTMLImageElement;
-        if (img) {
-          img.src = e.target?.result as string;
-          img.style.display = 'block';
+        if (this.imagePreview) {
+          this.imagePreview.nativeElement.src = this.image;
+          this.imagePreview.nativeElement.style.display = 'block';
         }
       };
       reader.readAsDataURL(file);
@@ -131,15 +144,53 @@ export class UserCreateComponent implements OnInit {
       this.showImagePreview = false;
     }
   }
+
   openImagePicker() {
-    const imageInput = document.getElementById('img-user') as HTMLInputElement;
-    if (imageInput) {
-      imageInput.click();
+    if (this.imgUser) {
+      this.imgUser.nativeElement.click();
     } else {
-      console.error('Elemento com ID "img-user" não encontrado.');
+      console.error('Elemento com ID "img-event" não encontrado.');
     }
   }
+
+
+  passwordStrength: string = '';
+
+  calculatePasswordStrength(password: string): string {
+    let strength = 0;
+
+    if (password.length >= this.passwordMinLength) strength += 1;
+    if (/[A-Z]/.test(password)) strength += 1;
+    if (/[a-z]/.test(password)) strength += 1;
+    if (/[0-9]/.test(password)) strength += 1;
+    if (/[\W]/.test(password)) strength += 1;
+
+    switch (strength) {
+      case 5: return 'forte';
+      case 4: return 'media';
+      default: return 'fraca';
+    }
+  }
+
+  onPasswordInput(): void {
+    const password = this.form.get('password')?.value;
+    this.passwordStrength = this.calculatePasswordStrength(password);
+  }
+
+  showPassword: boolean = false;
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+    const passwordField = document.getElementById('password') as HTMLInputElement;
+    if (this.showPassword) {
+      passwordField.type = 'text';
+    } else {
+      passwordField.type = 'password';
+    }
+  }
+
   validateFields() {
     return this.form.valid;
   }
+
 }
