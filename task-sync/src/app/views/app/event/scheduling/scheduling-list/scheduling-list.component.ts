662f0e08e824ca43a14ce8f6a423d3d4b33ed916
MichelecFernandes <michelecristinafr@gmail.com>
2024-10-19 08:34:28 -0300

    feat: implemnetacao do segundo grafico de barras (falta ajustar o css)

--- task-sync/src/app/views/app/event/scheduling/scheduling-list/scheduling-list.component.ts
+++ task-sync/src/app/views/app/event/scheduling/scheduling-list/scheduling-list.component.ts
@@ -16,10 +16,14 @@ import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
 import { MatPaginatorIntl, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { NgbModalRef, NgbModalOptions, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as echarts from 'echarts/core';
-import { PieChart, PieSeriesOption } from 'echarts/charts';
+import { BarChart, BarSeriesOption, PieChart, PieSeriesOption } from 'echarts/charts';
 import { TitleComponent, TooltipComponent, LegendComponent } from 'echarts/components';
 import { CanvasRenderer } from 'echarts/renderers';
-echarts.use([PieChart, TitleComponent, TooltipComponent, LegendComponent, CanvasRenderer]);
+echarts.use([BarChart, PieChart, TitleComponent, TooltipComponent, LegendComponent, CanvasRenderer]);
+import { CanvasJSChart } from '@canvasjs/angular-charts';import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+
+import { GridComponent } from 'echarts/components';
+echarts.use([GridComponent]);
 
 @Component({
   selector: 'task-sync-scheduling-list',
@@ -37,6 +41,7 @@ echarts.use([PieChart, TitleComponent, TooltipComponent, LegendComponent, Canvas
   styleUrl: './scheduling-list.component.css'
 })
 export class SchedulingListComponent implements OnInit {
+  
   fa = fontawesome;
   faAdd = faPlus;
   eventId: string = '';
@@ -57,6 +62,20 @@ export class SchedulingListComponent implements OnInit {
   schedulingCopy: ResponseScheduling[] = [];
   @ViewChildren('statusCard') statusCards!: QueryList<ElementRef>;
   modalRef: NgbModalRef | null = null;
+  chartOptions: any;
+
+  createChartOptions(data: any[]): any {
+    return {
+      animationEnabled: true,
+      title: {
+        text: "Meu Gráfico"
+      },
+      data: [{
+        type: "column",
+        dataPoints: data
+      }]
+    };
+  }
   constructor(
     private schedulingReadService: SchedulingReadService,
     private schedulingDeleteService: SchedulingDeleteService,
@@ -86,68 +105,19 @@ export class SchedulingListComponent implements OnInit {
       const from = page * pageSize + 1;
       const to = Math.min(from + pageSize - 1, length);
       return `${from} - ${to} de ${length}`;
-    };
-   
-  
-      const chartDom = document.getElementById('myChart') as HTMLDivElement;
-    
+    };     
+      const chartDom = document.getElementById('myChart') as HTMLDivElement;    
       if (!chartDom) {
         console.error('Elemento do gráfico não encontrado');
         return;
-      }
-    
-      const teste = document.getElementById('meuGrafico');
-      const myChart = echarts.init(teste);
-      const option: echarts.ComposeOption<PieSeriesOption> = {
-        title: {
-          text: 'Cronogramas',
-          left: 'center',
-          textStyle: {
-            color: 'red',
-          },
-        },
-        tooltip: {
-          trigger: 'item',
-        },
-        legend: {
-          bottom: '5%',
-          left: 'center',
-        },
-        series: [
-          {
-            name: 'Cronogramas',
-            type: 'pie',
-            radius: ['40%', '70%'],
-            avoidLabelOverlap: false,
-            itemStyle: {
-              borderRadius: 10,
-              borderColor: '#fff',
-              borderWidth: 2,
-            },
-            label: {
-              show: false,
-              position: 'center',
-            },
-            emphasis: {
-              label: {
-                show: true,
-                fontSize: 15,
-                fontWeight: 'bold',
-              },
-            },
-            data: [
-              { value: this.emAndamento, name: 'Em andamento' },
-              { value: this.emAberto, name: 'Em aberto' },
-              { value: this.concluido, name: 'Concluído' },
-            ],
-          },
-        ],
-      };
-
-      myChart.setOption(option);
+      } 
+          
+      this.loadCharts(); 
+      this.loadCharts2(); 
       
-
     
+
+      
   }
   
   loadSchedulings() {
@@ -159,76 +129,14 @@ export class SchedulingListComponent implements OnInit {
       this.emAberto = data.filter((item: ResponseScheduling) => item.status.toLowerCase() === 'em aberto').length;
       this.schedulingCopy = data;
       this.schedulings = data;
-      this.length = data.length;
-  
+      this.length = data.length;  
       this.applyDynamicStyles();
-      
-    
-        const chartDom = document.getElementById('myChart') as HTMLDivElement;
-      
-        if (!chartDom) {
-          console.error('Elemento do gráfico não encontrado');
-          return;
-        }
-      
+      this.loadCharts();
+      this.loadCharts2();
      
-      
-        const myChart = echarts.init(chartDom);
-
-        // // Define the options without the generic type
-        // const option: echarts.EChartsOption = {
-        //   title: {
-        //     text: 'Cronogramas',
-        //     left: 'center',
-        //     textStyle: {
-        //       color: 'red',
-        //     },
-        //   },
-        //   tooltip: {
-        //     trigger: 'item',
-        //   },
-        //   legend: {
-        //     bottom: '5%',
-        //     left: 'center',
-        //   },
-        //   series: [
-        //     {
-        //       name: 'Cronogramas',
-        //       type: 'pie',
-        //       radius: ['40%', '70%'],
-        //       avoidLabelOverlap: false,
-        //       itemStyle: {
-        //         borderRadius: 10,
-        //         borderColor: '#fff',
-        //         borderWidth: 2,
-        //       },
-        //       label: {
-        //         show: false,
-        //         position: 'center',
-        //       },
-        //       emphasis: {
-        //         label: {
-        //           show: true,
-        //           fontSize: 15,
-        //           fontWeight: 'bold',
-        //         },
-        //       },
-        //       data: [
-        //         { value: this.emAndamento, name: 'Em andamento' },
-        //         { value: this.emAberto, name: 'Em aberto' },
-        //         { value: this.concluido, name: 'Concluído' },
-        //       ],
-        //     },
-        //   ],
-        // };
-        
-        // Set the option on the chart
-        //myChart.setOption(option);
-    
-  
       const formData: { [key: string]: string[] } = {};
       this.schedulings.forEach(e => {
-        formData[`status${e.id}`] = [''];
+        formData[`status${e.id}`] = [e.status];
       });
       this.form = this.formBuilder.group(formData);
     });
@@ -254,6 +162,10 @@ export class SchedulingListComponent implements OnInit {
       }
     });
   }
+
+
+
+  
   openMyModal(content: any) {
     const options: NgbModalOptions = {
       backdropClass: 'app-session-modal-backdrop',
@@ -295,71 +207,12 @@ export class SchedulingListComponent implements OnInit {
       console.log(schedulingUpdate);
       await this.updateStatus.update(schedulingUpdate);
       this.toastrService.success('Cronograma atualizado com sucesso!');
-    
-
- 
       this.emAndamento = this.schedulings.filter((item: ResponseScheduling) => item.status.toLowerCase() === 'em andamento').length;
       this.concluido = this.schedulings.filter((item: ResponseScheduling) => item.status.toLowerCase() === 'finalizada').length;
-      this.emAberto = this.schedulings.filter((item: ResponseScheduling) => item.status.toLowerCase() === 'em aberto').length;
-
-          
-        const chartDom = document.getElementById('myChart') as HTMLDivElement;
-      
-        if (!chartDom) {
-          console.error('Elemento do gráfico não encontrado');
-          return;
-        }
-      
-        // const myChart = echarts.init(chartDom);
-      
-        // const option: echarts.EChartsOption = {
-        //   title: {
-        //     text: 'Cronogramas',
-        //     left: 'center',
-        //     textStyle: {
-        //       color: 'red'
-        //     }
-        //   },
-        //   tooltip: {
-        //     trigger: 'item',
-        //   },
-        //   legend: {
-        //     bottom: '5%',
-        //     left: 'center',
-        //   },
-        //   series: [
-        //     {
-        //       name: 'Cronogramas',
-        //       type: 'pie',
-        //       radius: ['40%', '70%'],
-        //       avoidLabelOverlap: false,
-        //       itemStyle: {
-        //         borderRadius: 10,
-        //         borderColor: '#fff',
-        //         borderWidth: 2,
-        //       },
-        //       label: {
-        //         show: false,
-        //         position: 'center',
-        //       },
-        //       emphasis: {
-        //         label: {
-        //           show: true,
-        //           fontSize: 15,
-        //           fontWeight: 'bold',
-        //         },
-        //       },
-        //       data: [
-        //         { value: this.emAndamento, name: 'Em andamento' },
-        //         { value: this.emAberto, name: 'Em aberto' },
-        //         { value: this.concluido, name: 'Concluído' },
-        //       ],
-        //     },
-        //   ],
-        // };
-      
-        // myChart.setOption(option);
-    } catch (error) {
+      this.emAberto = this.schedulings.filter((item: ResponseScheduling) => item.status.toLowerCase() === 'em aberto').length;      
+      this.loadCharts();
+      this.loadCharts2(); 
+      } catch (error) {
       this.toastrService.error('Erro. Cronograma não foi atualizado.');
     }
   }
@@ -397,6 +250,94 @@ export class SchedulingListComponent implements OnInit {
     this.schedulings = schedulings;
   }
 
+  loadCharts(){
+    const teste = document.getElementById('meuGrafico');
+    const myChart1 = echarts.init(teste);
+    const chart1: echarts.ComposeOption<PieSeriesOption> = {
+      title: {
+        text: 'Cronogramas',
+        left: 'center',
+        textStyle: {
+          color: 'black',
+        },
+      },
+      tooltip: {
+        trigger: 'item',
+      },
+      legend: {
+        bottom: '1%',
+        left: 'center',
+      },
+      series: [
+        {
+          name: 'Cronogramas',
+          type: 'pie',
+          radius: ['40%', '70%'],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: '#fff',
+            borderWidth: 2,
+          },
+          label: {
+            show: false,
+            position: 'center',
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: 15,
+              fontWeight: 'bold',
+            },
+          },
+          data: [
+            { value: this.emAndamento, name: 'Em andamento' },
+            { value: this.emAberto, name: 'Em aberto' },
+            { value: this.concluido, name: 'Concluído' },
+          ],
+        },
+      ],
+    };
+    myChart1.setOption(chart1);
+    
+  }
+
+
+
+  loadCharts2() {
+    const teste2 = document.getElementById('meuGrafico2');
+    const myChart12 = echarts.init(teste2);
+    const chart12: echarts.ComposeOption<BarSeriesOption> = {
+      title: {
+        text: 'Cronogramas xxxx',
+        left: 'center',
+        textStyle: {
+          color: 'black',
+        },
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Em andamento', 'Em aberto', 'Concluído']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          name: 'Cronogramas',
+          type: 'bar',
+          data: [this.emAndamento, this.emAberto, this.concluido]
+        }
+      ]
+    };
+    myChart12.setOption(chart12);
+  }
 
 
 
