c7dd3a627f6a88e2243ea965685674cf1e8d01f6
Lucas Severini <lucas_reiseverini@yahoo.com.br>
2024-10-19 09:23:05 -0300

    feat: iniciado criacao da nova tela para gerenciamento de constas

---
+++ task-sync/src/app/views/app/user/user-master/user-master.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task-sync-user-master',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-master.component.html',
+  styleUrl: './user-master.component.css'
+})
+export class UserMasterComponent {
+
+}
