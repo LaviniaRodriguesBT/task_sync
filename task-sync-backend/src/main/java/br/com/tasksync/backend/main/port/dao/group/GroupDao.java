3cff2c7f3c953277d8e203fb9dda073ae47db4c6
Lucas Severini <lucas_reiseverini@yahoo.com.br>
2024-10-29 19:00:54 -0300

    feat: iniciado implementação de groups

---
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/port/dao/group/GroupDao.java
@@ -0,0 +1,7 @@
+package br.com.tasksync.backend.main.port.dao.group;
+
+import br.com.tasksync.backend.main.domain.GroupModel;
+import br.com.tasksync.backend.main.port.dao.crud.CrudDao;
+
+public interface GroupDao extends CrudDao<GroupModel> {
+}
