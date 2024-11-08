0e36e1ced98e8c9be50e407c985d265c65ed2298
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-08 11:18:22 -0300

    test: finalizacao do caso de caminho feliz para o caso de teste de acesso como administrador

--- task-sync/test/specs/test.e2e.ts
+++ task-sync/test/specs/test.e2e.ts
@@ -2,16 +2,14 @@ import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page'
 import SecurePage from '../pageobjects/secure.page'
 
-describe('ACessar o website', () => {
-    it('Acessar a apgina de login de administrador', async () => {
+describe('Acessar o website', () => {
+    it('Acessar a pagina de login de administrador', async () => {
         await LoginPage.open();
         await browser.pause(500);
-        await LoginPage.clickButton();
-        await browser.pause(500);
-
-    //Criacao de mais testes
-
-
+        await LoginPage.clickButton('login');
+        await LoginPage.clickButton('admin');
+        await LoginPage.login('michelef@.com', '123', 'sign');
     })
+
 })
 
