a9c35172a4c6dcee4095a77a20a7ac8d0c1f900b
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-08 11:20:10 -0300

    test: finalizacao do caso de caminho feliz para o caso de teste de acesso como colaborador

--- task-sync/test/specs/test.e2e.ts
+++ task-sync/test/specs/test.e2e.ts
@@ -11,5 +11,12 @@ describe('Acessar o website', () => {
         await LoginPage.login('michelef@.com', '123', 'sign');
     })
 
+    it('Acessar a pagina de login de administrador', async () => {
+        await LoginPage.open();
+        await browser.pause(500);
+        await LoginPage.clickButton('login');
+        await LoginPage.clickButton('admin');
+        await LoginPage.login('michelef@.com', '123', 'sign');
+    })
 })
 
