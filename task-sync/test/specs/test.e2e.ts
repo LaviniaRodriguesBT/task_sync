38c20b91adde2a1019a044364740c8c69df482b3
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-12 13:58:38 -0300

    tests: melhorias nos codigos dos cenarios de testes

--- task-sync/test/specs/test.e2e.ts
+++ task-sync/test/specs/test.e2e.ts
@@ -4,7 +4,7 @@ import MainPage from '../pageobjects/main.page'
 import EventListPage from '../pageobjects/event-list.page'
 import SecurePage from '../pageobjects/secure.page'
 
-describe('Criar novo evento', () => {
+describe('Acessar a aplicação ', () => {
     it('Acessar a pagina de login de administrador', async () => {
         await LoginPage.open();
         await browser.pause(500);
@@ -12,13 +12,17 @@ describe('Criar novo evento', () => {
         await LoginPage.clickButton('admin');
         await LoginPage.login('michelef@.com', '123', 'sign');
     })
+})
 
+
+describe('Acessar a lista de eventos ', () => {
     it('Acessar a lista de eventos', async () => {
         await MainPage.clickButton('event');
         await MainPage.clickButton('list-events');
-
     })
+})
 
+describe('Acessar a pagina de criar novo evento', () => {
     it('Criar novo evento', async () => {
         await EventListPage.clickButton('create-event');
         await browser.pause(3000);
