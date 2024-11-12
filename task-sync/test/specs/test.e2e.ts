7e7a729122e6ef9f4bccfc7b987094bace294f81
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-12 13:50:41 -0300

    test: adicao dos tetses e2e que chegam ate a tela de cadastrar um novo evento

--- task-sync/test/specs/test.e2e.ts
+++ task-sync/test/specs/test.e2e.ts
@@ -1,8 +1,10 @@
 import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page'
+import MainPage from '../pageobjects/main.page'
+import EventListPage from '../pageobjects/event-list.page'
 import SecurePage from '../pageobjects/secure.page'
 
-describe('Acessar o website', () => {
+describe('Criar novo evento', () => {
     it('Acessar a pagina de login de administrador', async () => {
         await LoginPage.open();
         await browser.pause(500);
@@ -11,12 +13,15 @@ describe('Acessar o website', () => {
         await LoginPage.login('michelef@.com', '123', 'sign');
     })
 
-    it('Acessar a pagina de login de colaborador', async () => {
-        await LoginPage.open();
-        await browser.pause(500);
-        await LoginPage.clickButton('login');
-        await LoginPage.clickButton('colab');
-        await LoginPage.login('laviniabf@.com', '321', 'sign');
+    it('Acessar a lista de eventos', async () => {
+        await MainPage.clickButton('event');
+        await MainPage.clickButton('list-events');
+
+    })
+
+    it('Criar novo evento', async () => {
+        await EventListPage.clickButton('create-event');
+        await browser.pause(3000);
     })
 })
 
