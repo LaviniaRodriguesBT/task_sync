674b4dfc15edfffbaecc07f1c2610f362e197f09
MichelecFernandes <michelecristinafr@gmail.com>
2024-11-12 20:35:11 -0300

    test: correcao do metodos responsaveis por capturar os elementos da pagina

---
+++ task-sync/test/pageobjects/event-create.page.ts
@@ -0,0 +1,59 @@
+import { $ } from '@wdio/globals'
+import Page from './page';
+
+class EventCreatePage extends Page {
+
+    public getField(fieldName: string) {
+        return $(`.ft-field-${fieldName}`);
+    }
+
+    public async writeInput(fieldName: string, value: string) {
+    
+        const field = this.getField(fieldName);
+        await field.click();
+        await field.setValue(value);
+        await browser.pause(500);
+    }
+
+    public getButton(buttonName: string) {
+        return $(`.ft-btn-${buttonName}`);
+    }
+
+
+    public getFieldDate(buttonName: string) {
+        return $(`.ft-field-${buttonName}`);
+    }
+
+    public calendarioClick() {
+        return  $(`.calendar-icon`).click()
+    }
+
+    public async clickButton(buttonName: string) {
+        const button = this.getButton(buttonName);
+        await button.click();
+        await browser.pause(500);
+    }
+          
+    public async createEvent(
+        nameEvent: string, 
+        codeEvent: string,
+        descriptionEvent: string, 
+        businessEvent: string,
+        dateEvent: string,
+        starTimeEvent: string,
+        endTimeEvent: string, 
+        button: string) {
+        await this.writeInput('name', nameEvent);
+        await this.writeInput('code', codeEvent);
+        await this.writeInput('description', descriptionEvent);
+        await this.writeInput('business', businessEvent);
+        await this.writeInput('date', dateEvent);
+        await this.writeInput('starTime', starTimeEvent);
+        await this.writeInput('endTime', endTimeEvent);
+        await this.clickButton(button)
+    }
+
+
+}
+
+export default new EventCreatePage();
