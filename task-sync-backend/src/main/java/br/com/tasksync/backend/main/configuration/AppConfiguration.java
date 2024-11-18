334cbbd112263e6f0ee8f9806d9bc33c440eb50b
Lucas Severini <lucas_reiseverini@yahoo.com.br>
2024-11-18 18:42:22 -0300

    refactor: alterado perfil para de prod para sec, para teste e colocado sec em todas as chamadas para a mesma finalidade

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/configuration/AppConfiguration.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/configuration/AppConfiguration.java
@@ -10,10 +10,15 @@ import br.com.tasksync.backend.main.port.dao.scheduling.SchedulingDao;
 import br.com.tasksync.backend.main.port.dao.task.TaskDao;
 import br.com.tasksync.backend.main.port.dao.user.UserDao;
 import br.com.tasksync.backend.main.port.service.authentication.AuthenticationService;
+import br.com.tasksync.backend.main.port.service.authentication.JwtService;
 import br.com.tasksync.backend.main.port.service.user.UserService;
+import br.com.tasksync.backend.main.service.authentication.JwtAuthenticationServiceImplem;
+import br.com.tasksync.backend.main.service.authentication.JwtServiceImplem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.sql.Connection;
 
@@ -22,43 +27,43 @@ public class AppConfiguration {
 
 
     @Bean
-    @Profile("{prod, sec}")
+    @Profile({"prod","sec"})
     public UserDao getUserDao(final Connection connection) {
         return new UserPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
     public EventDao getEventDao(final Connection connection) {
         return new EventPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
     public TaskDao getTaskDao(final Connection connection) {
         return new TaskPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
     public SchedulingDao getSchedulingDao(final Connection connection) {
         return new SchedulingPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
     public ContractDao getContractDao(final Connection connection) {
         return new ContractPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
     public ActivityDao getActivityDao(final Connection connection) {
         return new ActivityPostgresDaoImplem(connection);
     }
 
     @Bean
-    @Profile("prod")
+    @Profile({"prod","sec"})
         public PlanDao getPlanDao(final Connection connection){
         return new PlanPostgresDaoImplem(connection);
     }
@@ -67,13 +72,13 @@ public class AppConfiguration {
     @Bean
     @Profile("sec")
     public AuthenticationService JwtAuthenticationService(final UserService userService) {
-        return new BasicAuthenticationServiceImpl(userService, passwordEncoder());
+        return new JwtAuthenticationServiceImplem(userService, passwordEncoder());
     }
 
     @Bean
     @Profile("sec")
     public JwtService jwtService() {
-        return new JwtServiceImpl();
+        return new JwtServiceImplem();
     }
 
     @Bean
