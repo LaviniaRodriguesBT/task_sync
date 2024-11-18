d07e528abf805ffadc3066d5cd129aef26c4857d
Lucas Severini <lucas_reiseverini@yahoo.com.br>
2024-11-18 18:43:11 -0300

    refactor: adicionado o perfil sec a classe AuthenticationDto

--- task-sync-backend/src/main/java/br/com/tasksync/backend/main/dto/AuthenticationDto.java
+++ task-sync-backend/src/main/java/br/com/tasksync/backend/main/dto/AuthenticationDto.java
@@ -3,10 +3,12 @@ package br.com.tasksync.backend.main.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Profile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Profile("sec")
 public class AuthenticationDto {
     private String password;
     private String email;
